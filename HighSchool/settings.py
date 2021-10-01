aa4e45ba98775f5baf8152db4e41ad866735a7dc
oumma <75848455+abdulselamT@users.noreply.github.com>
2021-10-01 10:39:21 +0300

    Update settings.py

--- HighSchool/settings.py
+++ HighSchool/settings.py
@@ -139,4 +139,4 @@ EMAIL_HOST = 'smtp.gmail.com'
 EMAIL_PORT = 587
 EMAIL_USE_TLS = True
 EMAIL_HOST_USER = 'abdulselam4245@gmail.com'
-EMAIL_HOST_PASSWORD = 'alhamdulilah3675@'
+EMAIL_HOST_PASSWORD = 'yrpfzvalhebfpdlb'
