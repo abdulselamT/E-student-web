6a05c7dce5794d68acf738010fe6dc9cfbcdf0d7
oumma <75848455+abdulselamT@users.noreply.github.com>
2021-10-01 09:36:01 +0300

    Update urls.py

--- student/urls.py
+++ student/urls.py
@@ -1,5 +1,7 @@
 from django.urls import path
 from . import views
+from django.contrib.auth import views as auth_views
+
 urlpatterns = [
     path('', views.studenthome,name="home"),
     path('teacherhome/', views.teacherhome,name="teacherhomepage"),
@@ -22,5 +24,23 @@ urlpatterns = [
     path('resubmit/<str:pk>/',views.resubmit,name='resubmit'),
     path('updatearrangment/<str:crs>/<str:sect>/<str:arr>/',views.teacherarrangementUpdate,name='resmit'),
     path('adminpage/',views.adminpage,name='adminpage'),
-    path('transfer/',views.transfer,name="transfer")
+    path('paschan',views.changepassword,name='passch'),
+    path('transfer/',views.transfer,name="transfer"),
+    path('reset_password/',
+     auth_views.PasswordResetView.as_view(template_name="student/password_reset.html"),
+     name="reset_password"),
+
+    path('reset_password_sent/', 
+        auth_views.PasswordResetDoneView.as_view(template_name="student/password_reset_sent.html"), 
+        name="password_reset_done"),
+
+    path('reset/<uidb64>/<token>/',
+     auth_views.PasswordResetConfirmView.as_view(template_name="student/password_reset_form.html"), 
+     name="password_reset_confirm"),
+
+    path('reset_password_complete/', 
+        auth_views.PasswordResetCompleteView.as_view(template_name="student/password_reset_done.html"), 
+        name="password_reset_complete"),
+
+
 ]
